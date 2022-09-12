670dfff73d7d6c91fde1f94e03bc67329741a2b9
Tobias Kempfer <tobias.kempfer@gmx.de>
2022-09-12 13:34:47 +0200

    updated tests

---
+++ tests/test_api_trace.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+import osm
+
+returncode=0
+def check(name,expected,got):
+    print("\033[4;30;37m",name,"\033[0;0m")
+    print("Expected:",expected)
+    print("Got:", got)
+    if expected == got:
+        print("\033[1;30;42m Passed \033[0;0m", flush=True)
+        sys.stdout.flush()
+    else:
+        print("\033[1;30;41m Failed \033[0;0m", flush=True)
+        global returncode
+        returncode=1
+
+def result(returncode):
+    if returncode==0:
+        print("\033[4;30;37m Overall Result: \033[0;0m")
+        print("\033[1;30;42m Passed \033[0;0m", flush=True)
+        return(0)
+    else:
+        print("\033[4;30;37m Overall Result: \033[0;0m")
+        print("\033[1;30;41m Failed \033[0;0m", flush=True)
+        return(1)
+
+trace=osm.trace("http://localhost:8000/osm",3244913)
+print(trace.get_points())
+
+
+result(returncode)
